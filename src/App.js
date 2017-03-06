376514032fcc2cae6891995abf40bd8dfcc0dfc2
Sai Chandramouli <schand31@asu.edu>
2017-03-06 00:08:38 -0800

    lumbering towards the solution

--- src/App.js
+++ src/App.js
@@ -4,13 +4,37 @@ import './App.css';
 
 
 class TextField extends Component {
+  constructor(props) {
+    super(props);
+    this.fieldName = props.fieldName;
+    this.handleNameChange = this.handleNameChange.bind(this);
+    this.handleBlur = this.handleBlur.bind(this);
+    this.state = {};
+  }
 
+  handleNameChange(e) {
+    this.setState({fieldValue: e.target.value});
+  }
+
+  handleBlur(e) {
+
+  }
+
+  render() {
+    return (
+      <div>
+        <label htmlFor={this.fieldName}>{this.fieldName}</label>
+        <input type="text" name={this.fieldName} id={this.fieldName} onInput={this.handleNameChange} onBlur={this.handleBlur}/>
+        <span id="{this.fieldName}+Editable">{this.state.fieldValue}</span>
+      </div>
+    );
+  }
 }
 class FormElements extends Component {
   render() {
     return (
       <div>
-        <TextField />
+        <TextField fieldName="Name" />
       </div>
     );
   }
